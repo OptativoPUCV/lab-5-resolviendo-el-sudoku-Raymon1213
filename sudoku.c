b6c66be902acbc31fb8b7a40d85e9132c457d0a4
Your Name <you@example.com>
2025-05-15 20:41:03 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -121,22 +121,24 @@ int is_final(Node* n){
 Node* DFS(Node* initial, int* cont){
   Stack* S = createStack();
   push(S, initial);
-  while (size(S) != 0) {
-        (*cont)++;
-        Node* n = top(S);
-        pop(S);
+  Node* topNode = first(S);
+  while ( topNode != NULL) {
+    (*cont)++;
+    Node* n = top(S);
+    pop(S);
+    if (is_final(n)) {
+      return n;
+    }
 
-        if (is_final(n)) {
-            return n;
-        }
+    List* adj = get_adj_nodes(n);
+    Node* aux = first(adj);
 
-        List* adj = get_adj_nodes(n);
-        Node* aux = first(adj);
-
-        while (aux) {
-            push(S, aux);
-            aux = next(adj);
-        }
+    while (aux) {
+      push(S, aux);
+      aux = next(adj);
+      }
+      free(n);
+      topNode = first(S);
     }
   return NULL;
 }
